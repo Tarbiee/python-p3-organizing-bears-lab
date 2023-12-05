53b7c23d03d3ac386334c27bd2652550a333a67d
Tarbiee <dorcusstaby@gmail.com>
2023-12-05 14:38:58 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 12, 'M', 'Brown', 'Quiet', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 10, 'M', 'Black', 'Rowdy', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 8, 'F', 'White', 'Friendly', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 15, 'M', 'Brown', 'Grumpy', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 7, 'F', 'Black', 'Playful', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 20, 'M', 'Green', 'Sly', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 9, 'F', 'Brown', 'Gentle', 1);
+INSERT INTO bears VALUES (8, NULL, 7, 'M', 'Gray', 'Sly', 1);
